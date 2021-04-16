f09f931e1f9ab89013d8aca358b78ea1d4f7ef95
henrique-vedois <75740946+henrique-vedois@users.noreply.github.com>
2021-04-15 21:26:55 -0300

    created templates and made the layout of the contact page

--- core/views.py
+++ core/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from .forms import ContatoForm
 
 def index(request):
     return render(request, 'index.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm(request.POST or none)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            celular = form.cleaned_data['telefone']
+            mensagem = form.cleaned_data['mensagem']
+
+            messages.success(request, 'E-mail envidado com sucesso')
+            form = ContatoForm()
+        else:
+            messages.success(request, 'Erro ao enviar o Email')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
 
 
 def produto(request):
